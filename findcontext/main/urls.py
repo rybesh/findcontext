ec417e18588488bb3b9c9fcc162e5cb9a99137e3
Ryan Shaw <ryanshaw@ischool.berkeley.edu>
2010-02-18 13:01:03 -0800

    Added index page.

--- findcontext/main/urls.py
+++ findcontext/main/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls.defaults import *
 from views import index, install, jetpack, sidebar
 
 urlpatterns = patterns('',
-   url(r'^/$', index),
+   url(r'^$', index),
    url(r'^install/$', install),
    url(r'^install/findcontext.js$', jetpack),
    url(r'^sidebar/$', sidebar),
